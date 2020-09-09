b6305a7ddaed118184511675dfed9443b64cfaf8
lxc <871673791@qq.com>
2020-09-09 11:08:15 +0800

    引入初始化样式

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-    <el-button type="primary" :loading="true">加载中</el-button>
     <router-view/>
   </div>
 </template>
 
 <style>
-
+@import './assets/css/base.css'
 </style>
