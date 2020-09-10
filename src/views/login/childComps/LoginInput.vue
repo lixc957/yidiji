a2997c09945c89a64700c5c98fc0349793003867
lxc <871673791@qq.com>
2020-09-10 11:09:39 +0800

    表单验证功能完成

--- src/views/login/childComps/LoginInput.vue
+++ src/views/login/childComps/LoginInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login-input">
-    <el-form :model="form">
-      <el-form-item class="input-item">
+    <el-form :model="form" :rules="rules" ref="ruleForm">
+      <el-form-item class="input-item" prop="phone">
         <el-input
           placeholder="请输入手机号"
           prefix-icon="el-icon-user"
@@ -10,7 +10,7 @@
         </el-input>
       </el-form-item>
 
-      <el-form-item class="input-item">
+      <el-form-item class="input-item" prop="password">
         <el-input
           placeholder="请输入密码"
           prefix-icon="el-icon-lock"
@@ -20,7 +20,7 @@
         </el-input>
       </el-form-item>
 
-      <el-form-item class="input-item">
+      <el-form-item class="input-item" prop="code">
         <el-row>
           <el-col :span="17">
             <el-input
@@ -39,14 +39,24 @@
       <el-form-item class="input-item">
         <el-checkbox v-model="isAgree" class="checkbox">
           <span>我已阅读并同意</span>
-          <el-link type="primary" class="link" :underline="false">用户协议</el-link>
+          <el-link type="primary" class="link" :underline="false"
+            >用户协议</el-link
+          >
           <span>和</span>
-          <el-link type="primary" class="link" :underline="false">隐私条款</el-link>
+          <el-link type="primary" class="link" :underline="false"
+            >隐私条款</el-link
+          >
         </el-checkbox>
       </el-form-item>
 
       <el-form-item class="input-item">
-        <el-button type="primary" :disabled="!isAgree" class="btn btn-login">登陆</el-button>
+        <el-button
+          type="primary"
+          :disabled="!isAgree"
+          class="btn btn-login"
+          @click="submitForm('ruleForm')"
+          >登陆
+        </el-button>
       </el-form-item>
 
       <el-form-item class="input-item">
@@ -66,9 +76,44 @@ export default {
         password: '',
         code: ''
       },
-      isAgree: false
+      isAgree: false,
+      rules: {
+        phone: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { min: 11, max: 11, message: '手机号长度为 11 位', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 12, message: '长度为 6 至 12 位', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { min: 4, max: 4, message: '验证码错误，请重新输入', trigger: 'blur' }
+        ]
+      }
     }
   },
+  methods: {
+    // 表单验证
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.$message({
+            message: '验证成功',
+            type: 'success',
+            duration: 2000
+          })
+        } else {
+          this.$message({
+            message: '验证失败',
+            type: 'error',
+            duration: 2000
+          })
+          return false
+        }
+      })
+    }
+  }
 }
 </script>
 
