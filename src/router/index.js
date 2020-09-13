487dc8f6ff8beed4d5a1f5ca0c574c4d45c371c3
lxc <871673791@qq.com>
2020-09-13 18:18:49 +0800

    首页用户退出

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Login from 'views/login/Login'
-const Home = () => import(/* webpackChunkName: 'home */ 'views/home/Home')
+const Home = () => import(/* webpackChunkName: 'home' */ 'views/home/Home')
 
 Vue.use(VueRouter)
 
@@ -26,5 +26,15 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
+const originalReplace = VueRouter.prototype.replace
+VueRouter.prototype.replace = function replace(location) {
+  return originalReplace.call(this, location).catch(err => err)
+}
 
 export default router
