84cf071869ce4043ae2ffd2938015609fee96756
lxc <871673791@qq.com>
2020-09-18 12:04:57 +0800

    题库页头部样式数据完成

--- src/views/home/childComps/question/HomeQuestion.vue
+++ src/views/home/childComps/question/HomeQuestion.vue
@@ -1,13 +1,90 @@
 <template>
-  <div class="question-list">题库列表</div>
+  <div class="question-list">
+    <el-card class="question-header">
+      <question-header 
+      ref="questionHeader" 
+      :search-form="searchForm"
+      :subject-list="subjectList"
+      :business-list="businessList"
+      :step-obj="stepObj"
+      :type-obj="typeObj"
+      :diff-obj="diffObj"
+      :status-obj="statusObj"
+       />
+    </el-card>
+  </div>
 </template>
 
 <script>
+import QuestionHeader from './QuestionHeader'
+
+import { getSubjectList } from 'network/subject'
+import { getBusinessList } from 'network/business'
+
 export default {
   name: 'HomeQuestion',
+  data () {
+    return {
+      searchForm: {
+        subject: '', //	否	int	学科id
+        step: '', //	否	string	题目阶段:1(初级),2(中级),3(高级)
+        enterprise: '', //	否	int	企业id
+        type: '', //	否	int	题目类型:1(单选),2(多选),3(简答)
+        difficulty: '', //	否	int	题目难度: 1(简单),2(一般),3(困难)
+        username: '', //	否	string	作者
+        status: '', //	否	int	状态:0(禁用),1(启用)
+        create_date: '', //	否	string	创建日期
+        title: '', //	否	string	标题名称
+        page: '', //	否	string	页码 默认为1
+        limit: '' //	否	string	页尺寸 默认为10
+      },
+      subjectList: [],
+      businessList: [],
+      stepObj: {
+        1: '初级',
+        2: '中级',
+        3: '高级'
+      },
+      typeObj: {
+        1: '单选',
+        2: '多选',
+        3: '简答'
+      },
+      diffObj: {
+        1: '简单',
+        2: '一般',
+        3: '困难'
+      },
+      statusObj: {
+        0: '禁用',
+        1: '启用'
+      }
+    }
+  },
+  created () {
+    this.getSubjectList()
+    this.getBusinessList()
+  },
+  methods: {
+    /**
+     * 	网络请求相关方法
+     */
+    async getSubjectList () {
+      const res = await getSubjectList({limit: Infinity})
+      const data = res.data.data.items
+      this.subjectList = data.map((item => ({rid: item.rid, name: item.name})))
+    },
+    async getBusinessList () {
+      const res = await getBusinessList({limit: Infinity})
+      const data = res.data.data.items
+      this.businessList = data.map((item => ({eid: item.eid, name: item.name})))
+    },
+  },
+  components: {
+    QuestionHeader
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
---
+++ src/views/home/childComps/question/QuestionHeader.vue
@@ -0,0 +1,179 @@
+<template>
+  <el-form
+    :model="searchForm"
+    ref="searchForm"
+    class="search-form"
+    label-width="100px"
+  >
+    <el-row>
+      <el-col :span="5">
+        <el-form-item label="学科">
+          <el-select
+            v-model.trim.lazy="searchForm.subject"
+            placeholder="请选择学科"
+          >
+            <el-option 
+            v-for="(item, index) in subjectList" 
+            :key="index" 
+            :value="item.rid" 
+            :label="item.name"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="5">
+        <el-form-item label="阶段">
+          <el-select
+            v-model.trim.lazy="searchForm.step"
+            placeholder="请选择阶段"
+          >
+            <el-option
+            v-for="(value, key, index) in stepObj" 
+            :key="index" 
+            :value="+key" :label="value"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="5">
+        <el-form-item label="企业">
+          <el-select
+            v-model.trim.lazy="searchForm.enterprise"
+            placeholder="请选择企业"
+          >
+            <el-option 
+            v-for="(item, index) in businessList" 
+            :key="index" 
+            :value="item.eid" 
+            :label="item.name"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="5">
+        <el-form-item label="题型">
+          <el-select
+            v-model.trim.lazy="searchForm.type"
+            placeholder="请选择题型"
+          >
+            <el-option
+            v-for="(value, key, index) in typeObj" 
+            :key="index" 
+            :value="+key" :label="value"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="4"></el-col>
+    </el-row>
+
+    <el-row>
+      <el-col :span="5">
+        <el-form-item label="难度">
+          <el-select
+            v-model.trim.lazy="searchForm.difficulty"
+            placeholder="请选择难度"
+          >
+            <el-option
+            v-for="(value, key, index) in diffObj" 
+            :key="index" 
+            :value="+key" :label="value"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="5">
+        <el-form-item label="作者">
+          <el-input v-model.trim.lazy="searchForm.username"></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="5">
+        <el-form-item label="状态">
+          <el-select
+            v-model.trim.lazy="searchForm.status"
+            placeholder="请选择状态"
+          >
+            <el-option
+            v-for="(value, key, index) in statusObj" 
+            :key="index" 
+            :value="+key" :label="value"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-col>
+      <el-col :span="5">
+        <el-form-item label="日期">
+          <el-date-picker 
+          v-model.trim.lazy="searchForm.create_date" 
+          type="date" 
+          placeholder="选择日期"
+          style="width: 100%">
+          </el-date-picker>
+        </el-form-item>
+      </el-col>
+      <el-col :span="4"></el-col>
+    </el-row>
+
+    <el-row>
+      <el-col :span="10">
+        <el-form-item label="标题">
+          <el-input v-model.trim.lazy="searchForm.title"></el-input>
+        </el-form-item>
+      </el-col>
+      <el-col :span="14">
+        <el-form-item label-width="10px">
+          <el-button type="primary">搜索</el-button>
+          <el-button>清除</el-button>
+          <el-button type="primary">+ 新增用户</el-button>
+        </el-form-item>
+      </el-col>
+    </el-row>
+  </el-form>
+</template>
+
+<script>
+export default {
+  name: 'QuestionHeader',
+  props: {
+    searchForm: {
+      type: Object,
+      default () {
+        return {}
+      }
+    },
+    subjectList: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    businessList: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    stepObj: {
+      type: Object,
+      default () {
+        return {}
+      }
+    },
+    typeObj: {
+      type: Object,
+      default () {
+        return {}
+      }
+    },
+    diffObj: {
+      type: Object,
+      default () {
+        return {}
+      }
+    },
+    statusObj: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
